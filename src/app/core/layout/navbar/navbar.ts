0495869acdfac9b86413a6b8c972ccdfef57d9c7
Leul Webshet <leulwebsdev@gmail.com>
2025-12-07 18:27:32 +0300

    feat(navbar): Add login modal with authentication form

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.html',
-  styles: ``,
+  standalone: true,
 })
 export class Navbar {
+  isLoginModalOpen = signal(false);
+  username = '';
+  password = '';
 
+  openLoginModal() {
+    this.isLoginModalOpen.set(true);
+  }
+
+  closeLoginModal() {
+    this.isLoginModalOpen.set(false);
+  }
+
+  onLogin() {
+    console.log('Login:', this.username, this.password);
+    // Add your login logic here
+    this.closeLoginModal();
+  }
 }
