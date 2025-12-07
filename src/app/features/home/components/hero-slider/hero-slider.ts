67264d92f4b03159405e37cf9fef9976f914cc38
Leul Webshet <leulwebsdev@gmail.com>
2025-12-07 16:53:23 +0300

    adding hero slider

--- src/app/features/home/components/hero-slider/hero-slider.ts
+++ src/app/features/home/components/hero-slider/hero-slider.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero-slider',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero-slider.html',
-  styles: ``,
+  standalone: true,
 })
-export class HeroSlider {
+export class HeroSlider implements OnInit, OnDestroy {
+  slides = [
+    'assets/images/hero_sliders/1.png',
+    'assets/images/hero_sliders/2.png',
+    'assets/images/hero_sliders/3.png',
+    'assets/images/hero_sliders/4.png',
+    'assets/images/hero_sliders/5.png',
+    'assets/images/hero_sliders/6.png',
+    'assets/images/hero_sliders/7.png',
+    'assets/images/hero_sliders/8.png',
+    'assets/images/hero_sliders/9.png',
+    'assets/images/hero_sliders/10.png',
+    'assets/images/hero_sliders/11.png',
+    'assets/images/hero_sliders/12.png',
+  ];
 
+  currentIndex = signal(0);
+  private intervalId?: number;
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.intervalId = window.setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = undefined;
+    }
+  }
+
+  nextSlide() {
+    this.currentIndex.set((this.currentIndex() + 1) % this.slides.length);
+  }
+
+  prevSlide() {
+    this.currentIndex.set(
+      (this.currentIndex() - 1 + this.slides.length) % this.slides.length
+    );
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex.set(index);
+    this.stopAutoSlide();
+    this.startAutoSlide();
+  }
+
+  getTransform() {
+    return `translateX(-${this.currentIndex() * 100}%)`;
+  }
 }
